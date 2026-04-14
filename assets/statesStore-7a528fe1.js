69edb26d76de7b28336acc137dedae3270902737
YU-FANG <jill83123@gmail.com>
2026-04-14 17:55:35 +0800

    Updates

--- assets/statesStore-7a528fe1.js
+++
@@ -1,1 +0,0 @@
-import{I as a}from"./index-22b5eba9.js";const i=a("states",{state:()=>({messages:[],state:{isLoading:!1,loadingIcon:""}}),actions:{pushToastMessage(e,t,s){if(e)this.messages.push({title:t,message:s,style:"bg-success"});else{const o=typeof s=="string"?[s]:s;this.messages.push({title:t,message:o.join(" 、 "),style:"bg-danger"})}}}});export{i as s};
---
+++ assets/statesStore-8fb2ae98.js
@@ -0,0 +1,1 @@
+import{I as a}from"./index-069841ce.js";const i=a("states",{state:()=>({messages:[],state:{isLoading:!1,loadingIcon:""}}),actions:{pushToastMessage(e,t,s){if(e)this.messages.push({title:t,message:s,style:"bg-success"});else{const o=typeof s=="string"?[s]:s;this.messages.push({title:t,message:o.join(" 、 "),style:"bg-danger"})}}}});export{i as s};
